41e7a8e0b05524d5ce598583893d2b4862084fe3
navneet <hello_navneet@hotmail.com>
2017-04-03 17:16:14 +0530

    Writes failing testcase form omdbservice.

--- spec/calculator.spec.js
+++
@@ -1,5 +0,0 @@
-describe('Calculator ', function() {
-  it('should should add two numbers...', function() {
-    expect(add(1, 2)).toBe(3);
-  });
-});
---
+++ spec/omdb/service.spec.js
@@ -0,0 +1,29 @@
+describe('omdb service', function() {
+  var movieData = {
+    "Search": [{
+      "Title": "Star Wars: Episode IV - A New Hope",
+      "Year": "1977",
+      "imdbID": "tt0076759",
+      "Type": "movie",
+      "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
+    }, {
+      "Title": "Star Wars: Episode I - The Phantom Menace",
+      "Year": "1999",
+      "imdbID": "tt0120915",
+      "Type": "movie",
+      "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BM2FmZGIwMzAtZTBkMS00M2JiLTk2MDctM2FlNTQ2OWYwZDZkXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg"
+    }, {
+      "Title": "Star Wars: Episode III - Revenge of the Sith",
+      "Year": "2005",
+      "imdbID": "tt0121766",
+      "Type": "movie",
+      "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
+    }],
+    "totalResults": "3",
+    "Response": "True"
+  };
+  it('should behave...Search movie data', function() {
+    var service = {};
+    expect(service.search('star wars')).toBe(movieData);
+  });
+});
