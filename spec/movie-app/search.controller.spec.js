7aca6d7f9114f8f3bcc3b421893354810dab192e
navneet <hello_navneet@hotmail.com>
2017-04-04 20:36:55 +0530

    Adds testcase for the search controller, which should not redirects.

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -1,12 +1,30 @@
 describe('Search controller', function() {
-  it('should redirect to the query result page for non-empty query', function() {
-    var $scope = {};
-    var $location = {};
-    $scope.search = function() {
-      $location.url = '/results?q=star%20wars';
+  var $scope;
+  var $location;
+
+  beforeEach(function() {
+    $location = {
+      url: ''
     };
+  });
+
+  $scope = {};
+
+  $scope.search = function() {
+    if ($scope.query) {
+      $location.url = '/results?q=star%20wars';
+    }
+  };
+
+  it('should redirect to the query result page for non-empty query', function() {
     $scope.query = 'star wars';
     $scope.search();
     expect($location.url).toBe('/results?q=star%20wars');
+  });
+
+  it('should not redirect to the query results for empty query', function() {
+    $scope.query = '';
+    $scope.search();
+    expect($location.url).toBe('');
   });
 });
