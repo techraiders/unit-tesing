9f52d7f2c19fc17939d25991b0871a4dfeb3bed4
navneet <hello_navneet@hotmail.com>
2017-04-04 21:02:58 +0530

    Creates mocked controller on the fly, inject _, _

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -2,29 +2,34 @@ describe('Search controller', function() {
   var $scope;
   var $location;
 
-  beforeEach(function() {
-    $location = {
-      url: ''
+  beforeEach(inject(function(_$controller_, _$location_) {
+    $scope = {};
+    $location = _$location_;
+
+    var fn = function($scope) {
+      $scope.search = function() {
+        if ($scope.query) {
+          $location.path('/results').search('q', $scope.query);
+        }
+      };
     };
-  });
 
-  $scope = {};
+    _$controller_(fn, {
+      $scope: $scope,
+      $location: $location
+    });
+  }));
 
-  $scope.search = function() {
-    if ($scope.query) {
-      $location.url = '/results?q=star%20wars';
-    }
-  };
 
   it('should redirect to the query result page for non-empty query', function() {
     $scope.query = 'star wars';
     $scope.search();
-    expect($location.url).toBe('/results?q=star%20wars');
+    expect($location.url()).toBe('/results?q=star%20wars');
   });
 
   it('should not redirect to the query results for empty query', function() {
     $scope.query = '';
     $scope.search();
-    expect($location.url).toBe('');
+    expect($location.url()).toBe('');
   });
 });
