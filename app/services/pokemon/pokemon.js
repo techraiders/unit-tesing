629111137a13ebdf58a1b550d7c79ba40e665481
navneet <hello_navneet@hotmail.com>
2017-04-06 20:16:55 +0530

    Adds test cases and findByName method that asynchronously gets Pokemon from real api.

--- app/services/pokemon/pokemon.js
+++ app/services/pokemon/pokemon.js
@@ -3,8 +3,17 @@
 
   // Define the component and controller we loaded in our test
   angular.module('api.pokemon', [])
-    .factory('Pokemon', function() {
+    .factory('Pokemon', function($http) {
+      var API = 'http://pokeapi.co/api/v2/pokemon/';
       var Pokemon = {};
+
+      // Spy on this method chained with callThrough() allows it to continue to continue to $http.get()
+      Pokemon.findByName = function(name) {
+        return $http.get(API + name)
+          .then(function(res) {
+            return res.data;
+          });
+      };
 
       return Pokemon;
     });
--- app/services/pokemon/pokemon.spec.js
+++ app/services/pokemon/pokemon.spec.js
@@ -1,16 +1,69 @@
 describe('Pokemon factory', function() {
-  var Pokemon;
+  var Pokemon, $q, $httpBackend;
 
-  // Load the api.pokemon module which we'll create next 
+  // Add Pokeapi endpoint
+  var API = 'http://pokeapi.co/api/v2/pokemon/';
+
+  // Add mocked Pokéapi response
+  var RESPONSE_SUCCESS = {
+    'id': 25,
+    'name': 'pikachu',
+    'sprites': {
+      'front_default': 'http://pokeapi.co/media/sprites/pokemon/25.png'
+    },
+    'types': [{
+      'type': {
+        'name': 'electric'
+      }
+    }]
+  };
+
   beforeEach(angular.mock.module('api.pokemon'));
 
-  //Inject the Pokemon service
-  beforeEach(inject(function(_Pokemon_) {
+  // Inject $q and $httpBackend for testing HTTP requests
+  beforeEach(inject(function(_Pokemon_, _$q_, _$httpBackend_) {
     Pokemon = _Pokemon_;
+    $q = _$q_;
+    $httpBackend = _$httpBackend_;
   }));
 
-  //Verify our factory exists
   it('should exist', function() {
     expect(Pokemon).toBeDefined();
   });
+
+  describe('findByName()', function() {
+    var result;
+
+    beforeEach(function() {
+      // Initialize our local result object to an empty object before each test
+      result = {};
+
+      // Spy on our service call but allow it to continue to its implementation
+      spyOn(Pokemon, "findByName").and.callThrough();
+    });
+
+    it('should return a Pokemon when called with a valid name', function() {
+      var search = 'pikachu';
+
+      // Declare the endpoint we expect our service to hit and provide it with our mocked return values
+      $httpBackend.whenGET(API + search).respond(200, $q.when(RESPONSE_SUCCESS));
+
+      expect(Pokemon.findByName).not.toHaveBeenCalled();
+      expect(result).toEqual({});
+
+      Pokemon.findByName(search)
+        .then(function(res) {
+          result = res;
+        });
+
+      // Flush pending HTTP requests
+      $httpBackend.flush();
+
+      expect(Pokemon.findByName).toHaveBeenCalledWith(search);
+      expect(result.id).toEqual(25);
+      expect(result.name).toEqual('pikachu');
+      expect(result.sprites.front_default).toContain('.png');
+      expect(result.types[0].type.name).toEqual('electric');
+    });
+  })
 });
