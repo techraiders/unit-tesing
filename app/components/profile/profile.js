bcec9220496a6e04da218e25abdc0cda96980ccf
navneet <hello_navneet@hotmail.com>
2017-04-07 00:51:50 +0530

    UI Router resolves a user using 'Users' Factory; injects this resolvedUser to controller, and assigns resolvedUser to user property.

--- app/components/profile/profile.js
+++ app/components/profile/profile.js
@@ -3,15 +3,22 @@
 
   // Define the component and controller we loaded in our test
   angular.module('components.profile', [])
-    .controller('ProfileController', function() {
+    .controller('ProfileController', function(resolvedUser) {
       var vm = this;
+      vm.user = resolvedUser;
     })
     .config(function($stateProvider) {
       $stateProvider
         .state('profile', {
           url: 'user/:id',
           templateUrl: 'components/profile/profile.html',
-          controller: 'ProfileController as pc'
+          controller: 'ProfileController as pc',
+          resolve: {
+            // Add resolvedUser with a call to Users using $stateParams
+            resolvedUser: function(Users, $stateParams) {
+              return Users.findById($stateParams.id);
+            }
+          }
         });
     });
 })();
--- app/components/profile/profile.spec.js
+++ app/components/profile/profile.spec.js
@@ -1,25 +1,62 @@
 describe('components.profile', function() {
   var $controller;
 
-  // Load ui.router and our component.profile module which we will create next
   beforeEach(angular.mock.module('ui.router'));
   beforeEach(angular.mock.module('components.profile'));
 
-  // Inject the $controller service
   beforeEach(inject(function(_$controller_) {
     $controller = _$controller_;
   }));
 
   describe('ProfileController', function() {
-    var ProfileController;
+    var ProfileController, singleUser;
 
     beforeEach(function() {
-      // Create and instance of our controller
-      ProfileController = $controller('ProfileController', {});
+      // Define singleUser and add resolvedUser as a dependency to our controller 
+      singleUser = {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob',
+        pokemon: {
+          name: 'growlithe'
+        }
+      };
+      ProfileController = $controller('ProfileController', {
+        resolvedUser: singleUser
+      });
     });
 
     it('should be defined', function() {
       expect(ProfileController).toBeDefined();
+    });
+  });
+
+  describe('Profile Controller with a valid resolved user', function() {
+    var ProfileController, singleUser;
+
+    beforeEach(function() {
+      // Mock a valid user
+      singleUser = {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        location: 'New York',
+        twitter: 'billybob',
+        pokemon: {
+          name: 'growlithe'
+        }
+      };
+
+      // Add the valid user as our resolved dependency
+      ProfileController = $controller('ProfileController', {
+        resolvedUser: singleUser
+      });
+    });
+
+    it('should set the view model user object to the resolvedUser', function() {
+      expect(ProfileController.user).toEqual(singleUser);
     });
   });
 });
